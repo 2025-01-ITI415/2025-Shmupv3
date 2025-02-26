9e7c8e93651e3bc2ac42739feac6646e40d1b706
Bill Crosbie <billcrosbie@users.noreply.github.com>
2025-02-25 21:53:00 -0500

    Add hero Script and fix ship rotation
    empty parent has no rotation. the chile ship model is rotated -90 around X

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    static public Hero S { get; private set; }  // Singleton property    // a
+
+    [Header("Inscribed")]
+    // These fields control the movement of the ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Dynamic")]
+    [Range(0, 4)]                                          // b
+    public float shieldLevel = 1;
+
+    void Awake()
     {
-        
+        if (S == null)
+        {
+            S = this; // Set the Singleton only if it’s null                  // c
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Pull in information from the Input class
+        float hAxis = Input.GetAxis("Horizontal");                            // d
+        float vAxis = Input.GetAxis("Vertical");                              // d
+
+        // Change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += hAxis * speed * Time.deltaTime;
+        pos.y += vAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Rotate the ship to make it feel more dynamic                       // e
+        transform.rotation = Quaternion.Euler(vAxis * pitchMult, hAxis * rollMult, 0);
     }
 }
